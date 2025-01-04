305da22dba90d4bfb3c6f1db62780e5978262333
Nikolay Smyk <157750278+SmykNikolay@users.noreply.github.com>
2025-01-04 12:30:37 +0300

    Commit message

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+
+// import { Layout } from 'antd';
+// import { Routing } from '../pages';
+
+export const App = () => {
+  return (
+    <div>
+    <h1>
+      Hello world
+    </h1>
+    </div>
+    // <Layout>
+    //   <Layout.Content>
+    //     <Routing />
+    //   </Layout.Content>
+    // </Layout>
+  );
+};
+
